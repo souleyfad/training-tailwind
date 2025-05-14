73c008e1b9c1bfff80cc83f1ea80b140700f2513
af.safad99 <af.soulemana@solcyb.com>
2025-05-14 12:26:02 +0000

    Programmes detail revisé

--- src/app/programmes/[id]/page.tsx
+++ src/app/programmes/[id]/page.tsx
@@ -1,25 +1,16 @@
 // app/programmes/[id]/page.tsx
 import { getProgrammeById } from '../../../lib/api';
 import { Programme } from '../../../lib/api';
+import { getProgrammes } from '../../../lib/api';
 import { notFound } from 'next/navigation';
 import Header from "@/components/layout/header";
 
-// 🟩 Type correct pour App Router
-interface ProgrammeDetailPageProps {
-  params: {
-    id: string;
-  };
-}
+export default async function ProgrammeDetailPage(props: {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await props.params;
 
-export default async function ProgrammeDetailPage({ params }: ProgrammeDetailPageProps) {
-    let programme: Programme | null = null;
-
-    try {
-        programme = await getProgrammeById(params.id);
-    } catch (err) {
-        console.error(err);
-    }
-
+    const programme = await getProgrammeById(id).catch(() => null);
     if (!programme) return notFound();
 
     return (
@@ -34,9 +25,17 @@ export default async function ProgrammeDetailPage({ params }: ProgrammeDetailPag
 
             <div>
                 <img src={programme.image} alt={programme.title} className="w-full h-128 object-cover rounded-md" />
-                <p className="flex text-sm item-center justify-center text-gray-500 mt-10 mb-10">{programme.date}</p>
+                <p className="flex text-sm items-center justify-center text-gray-500 mt-10 mb-10">{programme.date}</p>
                 <p className="text-gray-700 text-base whitespace-pre-line">{programme.message}</p>
             </div>
         </main>
     );
+}
+
+export async function generateStaticParams() {
+    const programmes = await getProgrammes();
+
+    return programmes.map((programme) => ({
+        id: programme.id,
+    }));
 }
